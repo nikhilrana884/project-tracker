1bc472f0f03722e6d1acedf2719ba3e0be9b76c5
Nikhil <nikhilrana884@gmail.com>
2022-08-06 22:11:04 +0530

    components setup

---
+++ project-tracker-frontend/frontend/src/components/ListProjectComponent.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import ProjectService from '../services/ProjectService'
+
+class ListProjectComponent extends Component {
+    
+}
+
+export default ListProjectComponent
---
+++ project-tracker-frontend/frontend/src/services/ProjectService.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const EMPLOYEE_API_BASE_URL = "http://localhost:8080";
+
+class ProjectService {
+
+}
+
+export default new ProjectService()
